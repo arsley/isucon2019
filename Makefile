c4286afa0f14b8fc991252eb325ca48f64587eb0
arsley <blue20will@gmail.com>
2019-09-07 13:26:38 +0900

    :tada: Dockerize :tada:

---
+++ Makefile
@@ -0,0 +1,23 @@
+up-db:
+	docker-compose up -d db
+	docker-compose exec db /sql/wait-for-it.sh db:3306
+	docker-compose exec db mysql -uroot -hlocalhost -e 'source /sql/00_create_database.sql'
+	docker-compose exec db mysql -uroot -hlocalhost -e 'source /sql/01_schema.sql'
+	docker-compose exec db mysql -uroot -hlocalhost -e 'source /sql/02_categories.sql'
+
+init-app:
+	docker-compose run --rm webapp npm i
+
+build-app:
+	docker-compose run --rm webapp npm run build
+
+up-app:
+	docker-compose up -d webapp
+
+up: up-db init-app up-app
+
+down:
+	docker-compose down
+
+stop:
+	docker-compose stop
--- sql/init.sh
+++ sql/init.sh
@@ -3,12 +3,12 @@ set -xe
 set -o pipefail
 
 CURRENT_DIR=$(cd $(dirname $0);pwd)
-export MYSQL_HOST=${MYSQL_HOST:-127.0.0.1}
-export MYSQL_PORT=${MYSQL_PORT:-3306}
-export MYSQL_USER=${MYSQL_USER:-isucari}
-export MYSQL_DBNAME=${MYSQL_DBNAME:-isucari}
-export MYSQL_PWD=${MYSQL_PASS:-isucari}
+export MYSQL_HOST=localhost
+export MYSQL_PORT=${MYSQL_PORT}
+export MYSQL_USER=${MYSQL_USER}
+export MYSQL_DBNAME=${MYSQL_DBNAME}
+export MYSQL_PWD=${MYSQL_PASS}
 export LANG="C.UTF-8"
 cd $CURRENT_DIR
 
-cat 01_schema.sql 02_categories.sql initial.sql | mysql --defaults-file=/dev/null -h $MYSQL_HOST -P $MYSQL_PORT -u $MYSQL_USER $MYSQL_DBNAME
+cat 01_schema.sql 02_categories.sql initial.sql.gz | mysql -h $MYSQL_HOST -u $MYSQL_USER
